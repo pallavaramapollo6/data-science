2252faa6dae9526cc26c6258934b7a61f4762338
Apollo <pallavaramapollo6@gmail.com>
2026-09-18 15:27:49 +0530

    Add data files and barchart code

---
+++ bar_chart.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data_file = "D:\\Notes\\DS\\data\\sales.xlsx"
+df = pd.read_excel(data_file)
+df.groupby("Region")["Sales"].sum().plot(kind="bar")
+plt.show()
+df.to_csv("output.csv", index=False)
+df.to_excel("output.xlsx", index=False)
